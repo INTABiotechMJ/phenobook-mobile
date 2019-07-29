fe0322fc493de41b4d8df6a63278774aff3f208e
Juan Manuel Crescente <you@example.com>
2019-07-29 11:16:52 -0300

    fixes

--- platforms/ios/www/assets/js/settings.js
+++
@@ -1,8 +0,0 @@
-$("#url").val(localStorage.getItem('phenobook_url'));
-$("#save-btn").click(function(){
-    localStorage.setItem('phenobook_url', $("#url").val());
-    window.location.href = "../index.html";
-});
-$("#cancel-btn").click(function(){
-    window.location.href = "../index.html";
-});
